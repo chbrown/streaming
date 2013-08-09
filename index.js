09a4d9a7ac2d6e7185657ff0110161dd29908677
Christopher Brown <io@henrian.com>
2013-08-09 09:58:23 -0500

    Add streaming.readToEnd

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ exports.Mapper = require('./lib/mapper');
 exports.Queue = require('./lib/queue');
 exports.Splitter = require('./lib/splitter');
 exports.Timeout = require('./lib/timeout');
+
+exports.readToEnd = function(stream, callback) {
+  /** Read a stream to the end, buffering all chunks into an array.
+
+  * `callback` Function Callback function with signature: function(err, [chunk_01, chunk_02, ...])
+
+  For example, to read all STDIN:
+
+      streaming.readToEnd(sys.stdin, function(err, chunks) {
+        if (err) throw err;
+
+        var input = chunks.join('');
+        console.log('Got input of length: ' + input.length);
+      });
+
+  */
+  var chunks = [];
+  return stream
+    .on('error', callback)
+    .on('data', function(chunk) {
+      chunks.push(chunk);
+    })
+    .on('end', function() {
+      callback(null, chunks);
+    });
+};
