3f552818fb9ec7c07561ba128f90691a251109c2
Christopher Brown <io@henrian.com>
2013-11-11 23:11:25 -0600

    Implement proper Filter (like the Array.filter to Array.map) to pair with Mapper

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'; /*jslint es5: true, node: true, indent: 2 */
 
+exports.Filter = require('./lib/filter');
 exports.Glob = require('./lib/glob');
 exports.json = require('./lib/json'); // for json.Parser and json.Stringifier
 exports.Mapper = require('./lib/mapper');
