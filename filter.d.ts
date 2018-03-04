2fdf51921bb9e4c5b86bff28c0921c31b4000e71
Christopher Brown <io@henrian.com>
2018-03-04 16:32:45 -0600

    Remove TypeScript-compiled output from source control

--- filter.d.ts
+++
@@ -1,6 +0,0 @@
-import { Transform } from 'stream';
-export declare class Filter<T> extends Transform {
-    protected testFn: (chunk: T) => boolean;
-    constructor(testFn: (chunk: T) => boolean);
-    _transform(chunk: T, encoding: string, callback: (error?: Error, outputChunk?: T) => void): void;
-}
